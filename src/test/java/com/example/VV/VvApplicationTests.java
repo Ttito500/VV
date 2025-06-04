70eed0571cd0afcc323fea8cbee75426813d48db
Tiago Tito <tiago.tito.sampaio@gmail.com>
2025-06-04 13:54:34 -0300

    initial commit

---
+++ src/test/java/com/example/VV/VvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
